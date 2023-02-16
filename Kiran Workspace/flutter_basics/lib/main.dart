d0eaf998d2c0aef212b6825bb7a15f6ade16fa7d
KiranKhadd29 <121477204+KiranKhadd29@users.noreply.github.com>
2023-02-16 12:45:37 +0530

    Flutter Container and Center

---
+++ Kiran Workspace/flutter_basics/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Basics Concepts',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text('Flutter Container'),
+        ),
+        body: Center(
+            child: Container(
+                width: 100,
+                height: 100,
+                color: Colors.grey,
+                child: Center(
+                  child: Text('This is center of Flutter Container'),
+                ))));
+  }
+}
