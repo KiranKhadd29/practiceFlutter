3385f707a7a263a08be290bf890e5e3a7bccbaad
KiranKhadd29 <121477204+KiranKhadd29@users.noreply.github.com>
2023-02-22 11:26:18 +0530

    Widget - Grid View: Count, Extemd, Builder

--- Kiran Workspace/flutter_basics/lib/main.dart
+++ Kiran Workspace/flutter_basics/lib/main.dart
@@ -45,6 +45,8 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     var arrnames = ['Kiran','Sayali','Kiya','Kiyansh','Siya','Siyansh'];
+    var arrColors = [Colors.blue, Colors.grey, Colors.deepOrange, Colors.green, Colors.red, Colors.black,
+      Colors.deepPurpleAccent, Colors.orange, Colors.pink];
     var todayTime = DateTime.now();
     return Scaffold(
         appBar: AppBar(
@@ -53,8 +55,65 @@ class _MyHomePageState extends State<MyHomePage> {
           title: Text('Flutter Basics'),
         ),
         body:
-      ////////// Date & Time Picker ////////////////////
 
+      ////////// Grid View Builder ////////////////////
+      GridView.builder(
+        itemBuilder: (context,index){
+        return Container(color :arrColors[index] );
+      },
+      itemCount: arrColors.length,
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 100,
+            crossAxisSpacing: 11,
+            mainAxisSpacing: 11
+      ),
+
+        ),
+
+
+
+      ////////// Grid View ////////////////////
+      /*
+      GridView.count(
+        crossAxisCount: 3,
+        crossAxisSpacing: 11,
+        mainAxisSpacing: 11,
+      children: [
+        Container( color:arrColors[0]),
+        Container( color:arrColors[1]),
+        Container( color:arrColors[2]),
+        Container( color:arrColors[3]),
+        Container( color:arrColors[4]),
+        Container( color:arrColors[5]),
+        Container( color:arrColors[6]),
+        Container( color:arrColors[7]),
+        Container( color:arrColors[8]),
+      ],
+      )
+      */
+
+      ////////// Grid View Extent ////////////////////
+      /*
+      GridView.extent(
+      maxCrossAxisExtent: 100,
+      crossAxisSpacing: 11,
+      mainAxisSpacing: 11,
+      children: [
+        Container( color:arrColors[0]),
+        Container( color:arrColors[1]),
+        Container( color:arrColors[2]),
+        Container( color:arrColors[3]),
+        Container( color:arrColors[4]),
+        Container( color:arrColors[5]),
+        Container( color:arrColors[6]),
+        Container( color:arrColors[7]),
+        Container( color:arrColors[8]),
+      ],
+    )
+        */
+
+      ////////// Date & Time Picker ////////////////////
+      /*
       Center(
         child: Container(
           child: Column(
@@ -93,7 +152,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
       )
-      
+      */
 
       ////////// Get Current Date & Time ////////////////////
       /*
