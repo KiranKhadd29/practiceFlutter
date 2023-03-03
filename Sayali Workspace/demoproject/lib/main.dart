13282fd0199e9bb9c81f1f9516dda4a1ee61cb51
saya1605 <sayalikandhare16@hotmail.com>
2023-03-03 12:20:02 +0530

    Basic calculator

--- Sayali Workspace/demoproject/lib/main.dart
+++ Sayali Workspace/demoproject/lib/main.dart
@@ -1180,7 +1180,8 @@ class BottomMenu extends StatelessWidget {
 }
 
  */
-//Understanding of stateful and stateless widgets
+//Understanding of stateful and stateless widgets Counter app
+/*
 class MyHomePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -1209,6 +1210,87 @@ class _MyHomeState extends State<MyHomePage>{
           ],
         ),
       ),
+    );
+  }
+}
+*/
+//Arithmetic app
+class MyHomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyHomeState();
+  }
+}
+class _MyHomeState extends State<MyHomePage>{
+  var no1Controller = TextEditingController();
+  var no2Controller = TextEditingController();
+  var result = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculator'),
+      ),
+      body: Container(
+        color: Colors.blue.shade100,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                keyboardType: TextInputType.number,
+                controller: no1Controller,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                controller: no2Controller,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(21.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(onPressed: (){
+                      var no1 = int.parse(no1Controller.text.toString());
+                      var no2 = int.parse(no2Controller.text.toString());
+                      var sum = no1 + no2;
+                      result = "The sum of $no1 and $no2 is $sum";
+                      setState(() {});
+                    }, child: Text('Add')),
+                    ElevatedButton(onPressed: (){
+                      var no1 = int.parse(no1Controller.text.toString());
+                      var no2 = int.parse(no2Controller.text.toString());
+                      var subtraction = no1 - no2;
+                      result = "The difference of $no1 and $no2 is $subtraction";
+                      setState(() {});
+                    }, child: Text('Sub')),
+                    ElevatedButton(onPressed: (){
+                      var no1 = int.parse(no1Controller.text.toString());
+                      var no2 = int.parse(no2Controller.text.toString());
+                      var multiplication = no1 * no2;
+                      result = "The multiplication of $no1 and $no2 is $multiplication";
+                      setState(() {});
+                    }, child: Text('Mul')),
+                    ElevatedButton(onPressed: (){
+                      var no1 = int.parse(no1Controller.text.toString());
+                      var no2 = int.parse(no2Controller.text.toString());
+                      var division = no1 / no2;
+                      result = "The division of $no1 and $no2 is ${division.toStringAsFixed(2)}";
+                      setState(() {});
+                    }, child: Text('Div'))
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(21.0),
+                child: Text(result,style: TextStyle(fontSize: 25,color: Colors.deepOrange),),
+
+              )
+            ],
+          ),
+        ),
+      )
     );
   }
 }
